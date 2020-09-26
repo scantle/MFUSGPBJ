6b556643dcc8355753f27efe5b11e3786226c930
scantle <leland@scantle.com>
2020-09-26 11:10:14 -0400

    Added both alternative streambed conductance equations from Mehl and Hill (2010) to calc_conductance. Examples not provided.

--- R/calc_conductance.R
+++ R/calc_conductance.R
@@ -4,9 +4,9 @@
 #'
 #' @param swdf DataFrame/Geometry of node barycentric coordinates as returned by
 #'   \code{\link{calc_stream_voronoi_weights}}
-#' @param k_streambed numeric, streambed hydraulic conductivity
+#' @param k_str numeric, streambed hydraulic conductivity
 #' @param str_width numeric, stream width
-#' @param stream_thick numeric, streambed thickness (head loss zone)
+#' @param str_thick numeric, streambed thickness (head loss zone)
 #'
 #' @return numeric, conductance
 #' @author Leland Scantlebury
@@ -26,10 +26,10 @@
 #' swdf <- stream_elev_from_slope(swdf = swdf, slope = 0.0015, initial_elev = 50)
 #'
 #' #-- Calculate conductances
-#' swdf$Conductance <- calc_conductance_modflow(swdf, k_streambed = 1,
+#' swdf$Conductance <- calc_conductance_modflow(swdf, k_str = 1,
 #'                                              str_width = 1, thickness = 0.5)
-calc_conductance_modflow <- function(swdf, k_streambed, str_width, thickness) {
-  cond <- (k_streambed * swdf$Length * str_width) / thickness
+calc_conductance_modflow <- function(swdf, k_str, str_width, str_thick) {
+  cond <- (k_str * swdf$Length * str_width) / str_thick
   return(cond)
 }
 #-------------------------------------------------------------------------------------------------#
@@ -37,9 +37,9 @@ calc_conductance_modflow <- function(swdf, k_streambed, str_width, thickness) {
 #' Calculate classic MODFLOW-style stream conductance (per unit length)
 #' Per Unit Length Conductance = width x streambed hydraulic conductivity / streambed thickness
 #'
-#' @param k_streambed numeric, streambed hydraulic conductivity
+#' @param k_str numeric, streambed hydraulic conductivity
 #' @param str_width numeric, stream width
-#' @param thickness numeric, streambed thickness (head loss zone)
+#' @param str_thick numeric, streambed thickness (head loss zone)
 #'
 #' @return numeric, conductance
 #' @author Leland Scantlebury
@@ -59,9 +59,60 @@ calc_conductance_modflow <- function(swdf, k_streambed, str_width, thickness) {
 #' swdf <- stream_elev_from_slope(swdf = swdf, slope = 0.0015, initial_elev = 50)
 #'
 #' #-- Calculate conductances
-#' swdf$Conductance <- calc_conductance_modflow_perLen(k_streambed = 1, str_width = 1,
+#' swdf$Conductance <- calc_conductance_modflow_perLen(k_str = 1, str_width = 1,
 #'                                                     thickness = 0.5)
-calc_conductance_modflow_perLen <- function(k_streambed, str_width, thickness) {
-  cond <- (k_streambed * str_width) / thickness
+calc_conductance_modflow_perLen <- function(k_str, str_width, str_thick) {
+  cond <- (k_str * str_width) / str_thick
+  return(cond)
+}
+
+#' Calculate conductance using Mehl & Hill (2010) Equation 3
+#'
+#' Vectorized, non-constant values should be same length as swdf.
+#'
+#' @param swdf DataFrame/Geometry of node barycentric coordinates as returned by
+#'   \code{\link{calc_stream_voronoi_weights}}
+#' @param k_str numeric, streambed hydraulic conductivity
+#' @param kv_node numeric, vertical hydraulic conductivity of the aquifer material
+#' @param str_width numeric, stream width
+#' @param str_thick numeric, streambed thickness (head loss zone)
+#' @param node_thick numeric, thickness of finite difference cell (node)
+#' @param node_area Planar cross-sectional area of cell (node)
+#'
+#' @return numeric, conductance
+#' @author Leland Scantlebury
+#' @export calc_conductance_MHEq3
+#'
+#' @examples
+calc_conductance_MHEq3 <- function(swdf, k_str, kv_node, str_width, str_thick, node_thick, node_area) {
+  area <- str_width * swdf$Length
+  b2 <- node_thick / 2
+  cond <- ((str_thick/(k_str*area)) + ((b2-str_thick)/(kv_node*node_area)))^-1
+  return(cond)
+}
+
+#' Calculate conductance using Mehl & Hill (2010) Equation 4
+#'
+#' Vectorized, non-constant values should be same length as swdf.
+#'
+#' @param swdf DataFrame/Geometry of node barycentric coordinates as returned by
+#'   \code{\link{calc_stream_voronoi_weights}}
+#' @param k_str numeric, streambed hydraulic conductivity
+#' @param kv_node numeric, vertical hydraulic conductivity of the aquifer material
+#' @param str_width numeric, stream width
+#' @param str_thick numeric, streambed thickness (head loss zone)
+#' @param node_thick numeric, thickness of finite difference cell (node)
+#'
+#' @return numeric, conductance
+#' @references Mehl, S. and Hill, M.C. (2010) Grid-size dependence of Cauchy boundary conditions used to
+#' simulate stream-aquifer interactions. Advances in Water Resources 33. 430-442.
+#' @author Leland Scantlebury
+#' @export calc_conductance_MHEq4
+#'
+#' @examples
+calc_conductance_MHEq4 <- function(swdf, k_str, kv_node, str_width, str_thick, node_thick) {
+  area <- str_width * swdf$Length
+  b2 <- node_thick / 2
+  cond <- ((str_thick/(k_str*area)) + ((b2-str_thick)/(kv_node*area)))^-1
   return(cond)
 }
